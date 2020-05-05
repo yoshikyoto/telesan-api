d7c5ae049481e7d1a885683fca01ae449bd76beb
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2020-05-05 14:18:22 +0900

    postとgetを受け付けるように

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
@@ -13,6 +14,8 @@ import (
 func main() {
 	migrate()
 	router := gin.Default()
+	router.GET("/monsters", getMonsters)
+	router.POST("/monster", postMonster)
 	router.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Welcome")
 	})
@@ -36,6 +39,48 @@ func migrate() (err error){
 	return nil
 }
 
+type getMonstersQuery struct {
+	Names string `form:"names" binding:"required"`
+}
+
+func getMonsters(c *gin.Context) {
+	var query getMonstersQuery
+
+	if err := c.ShouldBindQuery(&query); err != nil {
+		c.JSON(400, gin.H{})
+		return
+	}
+
+	fmt.Println(query.Names)
+	names := strings.Split(query.Names, ",")
+	fmt.Println(names)
+
+	db, _ := connectDb()
+	defer db.Close()
+
+	var monsters []Monster
+	db.Where("name IN (?)", names).Find(&monsters)
+	c.JSON(200, monsters)
+}
+
+func postMonster(c *gin.Context) {
+	var query Monster
+
+	if err := c.BindJSON(&query); err != nil {
+		fmt.Println(err)
+		c.JSON(400, gin.H{})
+		return
+	}
+
+	db, _ := connectDb()
+	defer db.Close()
+	db.Where(Monster{Name: query.Name}).Assign(Monster{
+		Health:  query.Health,
+		Attack:  query.Attack,
+		Defence: query.Defence,
+	}).FirstOrCreate(&Monster{})
+}
+
 func connectDb() (db *gorm.DB, err error) {
 	// Heroku には DATABASE_URL というのが設定されている
 	url := os.Getenv("DATABASE_URL")
@@ -49,8 +94,8 @@ func connectDb() (db *gorm.DB, err error) {
 }
 
 type Monster struct {
-	Name string
-	Hp int
-	Attack int
-	Defence int
+	Name    string `gorm:"primary_key" json:"name" binding:"required"`
+	Health  int    `json:"health" binding:"required"`
+	Attack  int    `json:"attack" binding:"required"`
+	Defence int    `json:"defence" binding:"required"`
 }
